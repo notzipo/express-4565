f290a8b5834482833a5b30ff0f40eea4f84e067a
notzipo <notzipo@msn.com>
2026-06-12 11:31:54 +0700

    add Storage for k8s

--- backend/server.js
+++ backend/server.js
@@ -104,7 +104,7 @@ export async function startServer() {
 
   return new Promise((resolve) => {
     const server = app.listen(config.serverPort, () => {
-      console.log(`🚀 [QNAP-Interface Service] listening on http://localhost:${config.serverPort}`);
+      console.log(`🚀 QNAP-Interface Service listening on http://localhost:${config.serverPort}`);
       resolve(server);
     });
   });
